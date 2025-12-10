6fed25657783d0c74be914c62118ae6b72b7b344
Amnon Manneberg <148687878+Amnonman@users.noreply.github.com>
2025-12-10 12:01:43 +0200

    v1.0.11: Simplified CSS since card names now use span

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -58,19 +58,12 @@ class Plugin
             display: none !important;
         }
         /* Hide "The following addresses will be used on the checkout page by default." text */
-        /* Target the specific p element that WooCommerce adds */
+        /* Now safe to use since address cards use <span> for names, not <p> */
         .woocommerce-MyAccount-content > p:first-of-type,
         .woocommerce-MyAccount-content-wrapper p:first-of-type,
         .woocommerce-edit-address > p:first-of-type,
-        .e-my-account-tab__address > p:first-of-type,
-        .woocommerce-account .woocommerce > p:first-of-type,
-        .woocommerce-MyAccount-content-wrapper [data-elementor-type="container"] > p:first-of-type {
+        .e-my-account-tab__address > p:first-of-type {
             display: none !important;
-        }
-        /* Ensure our address card content is never hidden */
-        .address-card p,
-        [id^="hp-address-card-picker"] p {
-            display: block !important;
         }
         </style>
         <?php
